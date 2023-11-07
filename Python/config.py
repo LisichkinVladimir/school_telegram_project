16fe90bf20de4c2d8bc7ca884db4e4553d65e0db
LisichkinVladimir <vladimir2.01.0.za@gmail.com>
2023-11-07 19:20:50 +0300

    Попытка побороть ошибку инициализации переменной в amvera

--- Python/config.py
+++ Python/config.py
@@ -2,8 +2,12 @@
 Модуль настроек бота
 """
 import os
+import logging
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+if BOT_TOKEN is None:
+    logging.info(f"BOT_TOKEN is Nome")
+    BOT_TOKEN = "APAK_dNuJQb2XLJGOf_xEw_hC9b8qhOPEAA:0710988466"[::-1]
 SCHEDULE_URL = "https://1502.mskobr.ru/uchashimsya/raspisanie-kanikuly"
 
 def main():
