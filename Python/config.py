4ad0f6d246b9fc274e77e8ee4c93a37b48ad96b8
LisichkinVladimir <alisichkin@sportmaster.ru>
2024-01-24 19:47:51 +0300

    Изменение места расположения данных бота

--- Python/config.py
+++ Python/config.py
@@ -27,12 +27,29 @@ def get_data_path() -> str:
     """
     Место расположения данных приложения
     """
-    path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    # Место расположения данных приложения
-    db_path = f"{path}/data/"
+    db_path = os.getenv("PERSISTENCE_MOUNT")
+    if db_path is None:
+        db_path = "../&ScriptDir/data"
+    if "&ScriptDir" in db_path:
+        path = os.path.dirname(os.path.abspath(__file__))
+        str = db_path.partition("&ScriptDir")
+        prefix = str[0]
+        postfix = str[2]
+        for s in prefix.split("/"):
+            if s == "..":
+                path = os.path.dirname(path)
+        db_path = path + postfix
+    logging.info(f"db_path:{db_path}")
     if not os.path.exists(db_path):
         # создаем директорию
         os.mkdir(db_path)
+    else:
+        files = [f for f in os.listdir(db_path) if os.path.isfile(os.path.join(db_path, f))]
+        logging.info(f"files:\n{files}")
+        # Временный код
+        if os.path.exists("/data"):
+            files = [f for f in os.listdir("/data") if os.path.isfile(f)]
+            logging.info(f"files:\n{files}")
     return db_path
 
 def main():
